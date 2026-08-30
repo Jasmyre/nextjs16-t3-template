2c220c7fd90fc60802b84bb4a2b02ba1506d7090
Jasmyre <lanuzajasmyreandrei@gmail.com>
2026-08-30 22:14:49 +0800

    refactor: improve code formatting and readability across multiple files

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,11 @@
-export const publicRoutes = [
-  "/",
-  "/maintenance",
-];
+export const publicRoutes = ["/", "/maintenance"];
 
-export const authRoutes = ["/auth", "/auth/error", "/api/auth/callback/google", "/api/auth/callback/github"];
+export const authRoutes = [
+  "/auth",
+  "/auth/error",
+  "/api/auth/callback/google",
+  "/api/auth/callback/github",
+];
 
 export const apiAuthPrefix = "/api/auth";
 
